de12673bdd29ef0075b218badff1796253874daf
Ronny Dahl <ronny.dahl@agilis.no>
2018-03-20 11:00:27 +0100

    Some message

--- HellowRold/HellowRold/Program.cs
+++ HellowRold/HellowRold/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HellowRold
 {
@@ -10,6 +6,7 @@ namespace HellowRold
     {
         static void Main(string[] args)
         {
+            Console.Write("teete");
         }
     }
 }
